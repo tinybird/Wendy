5a54770b56f89ef6af64e5d669e3b7ef32349525
Richard Hult <richard@tinybird.com>
2011-10-16 15:17:19 +0200

    Add vendor ID setting

--- wendy/settings.rb
+++ wendy/settings.rb
@@ -5,8 +5,9 @@ class Settings
   @admin_email_addresses = []
   @itc_username = 'na'
   @itc_password = 'na'
+  @itc_vendorid = 'na'
 
   class << self
-    attr_accessor :sender, :sleep_duration, :report_email_addresses, :admin_email_addresses, :itc_username, :itc_password
+    attr_accessor :sender, :sleep_duration, :report_email_addresses, :admin_email_addresses, :itc_username, :itc_password, :itc_vendorid
   end
 end
