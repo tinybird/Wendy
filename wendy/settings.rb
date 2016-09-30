b3f6dd931de4e1ea7dd4c4f037727dfdbc64cb63
Richard Hult <richard@tinybird.com>
2016-09-30 20:11:05 +0200

    Add setting for the account number

--- wendy/settings.rb
+++ wendy/settings.rb
@@ -7,10 +7,11 @@ class Settings
   @itc_username = 'na'
   @itc_password = 'na'
   @itc_vendorid = 'na'
+  @itc_account = 'na'
 
   class << self
     attr_accessor :sender, :sleep_duration
     attr_accessor :report_email_addresses, :report_groups, :admin_email_addresses
-    attr_accessor :itc_username, :itc_password, :itc_vendorid
+    attr_accessor :itc_username, :itc_password, :itc_vendorid, :itc_account
   end
 end
